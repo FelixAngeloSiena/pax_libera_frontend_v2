dad6803ad7cd1b44929ebac9c17c2a6c452ea042
FelixAngeloSiena <sienadeveloper29@gmail.com>
2023-04-08 17:01:39 +0800

    intial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Dashboard from './components/dashboard/Dashboard';
+import '../src/assets/css/Styles.css'
+
+
+function App() {
+  return (
+    <div className="body">
+       <Dashboard/>
+    </div>
+  )
+ 
+}
+
+export default App;
